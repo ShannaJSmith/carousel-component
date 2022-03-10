5c5fa9d8f3c4c043b79564f266363b619de32eb5
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-10 14:41:37 -0500

    npm i react-elastic-carousel and import in app

--- carousel-component/src/App.js
+++ carousel-component/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Carousel from 'react-elastic-carousel';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Carousel></Carousel>
+    </div>
+  );
 }
 
 export default App;
